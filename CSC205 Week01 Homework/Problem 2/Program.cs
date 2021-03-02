75386fa445a5d34dbda306e5237e9e505ed36c1a
Jonathan Staub <jonathan.staub@protonmail.com>
2021-03-01 17:54:40 -0800

    Week01 Homework
    Here is my solutions to the Week 1 homework assignments for class CSC205 "Application Development in C#"

---
+++ CSC205 Week01 Homework/Problem 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Time.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+            int minute = 36;
+
+            int minutesElapsed = (hour * 60) + (minute);
+            Console.WriteLine($"Number of minutes since midnight: {minutesElapsed}");
+
+            int secondsElapsed = 60*((hour * 60) + (minute));
+            Console.WriteLine($"Number of seconds since midnight: {secondsElapsed}");
+
+            int secondsRemaining = 86400 - secondsElapsed;
+            Console.WriteLine($"Number of seconds remaining in the day: {secondsRemaining}");
+
+            int dayPerCent = (secondsElapsed * 100) / 86400;
+            Console.WriteLine($"Percentage of the day that has passed: {dayPerCent}%");
+
+            //Commas added for clarity but would need to be removed for use in code
+            //sbyte sbyte1 = -44;
+            //sbyte sbyte2 = 97;
+            //sbyte sbyte3 = 112;
+            //sbyte sbyte4 = -115;
+            //byte byte1 = 224;
+            //short short1= 1990;
+            //short short2 = -10000;
+            //short short3 = 20000;
+            //ushort ushort1 = 52130;
+            //int int1 = -1,000,000;
+            //int int2 = 4,825,932;
+            //int int4 = 970,700,000;
+            //ulong ulong1 = 123,456,789,123,456,789;
+
+            Num(2011);
+            Num(1988);
+        }
+        public static void Num(int num)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"The four digit number to be analyzed is: {num}");
+            int a = num % 10;
+            int b = (num / 10) % 10;
+            int c = (num / 100) % 10;
+            int d = (num / 1000) % 10;
+
+            int sumOfDigits = a + b + c + d;
+            Console.WriteLine($"The sum of the digits is: {sumOfDigits}");
+            Console.WriteLine($"The number reversed is: {a}{b}{c}{d}");
+            Console.WriteLine($"The number with the last digit first is: {a}{d}{c}{b}");
+            Console.WriteLine($"The number with inner digits switched is: {d}{b}{c}{a}");
+
+        }
+    }
+}
---
+++ CSC205 Week01 Homework/ThinkSharp Exercise2.3/Program.cs
@@ -0,0 +1,41 @@
+﻿/*   Think Sharp Exercise 2.3 pages 21-22.
+
+  1) Following the example in Section 2.6, create variables named hour, minute and second, and assign them values that are 
+     roughly the current time. Use a 24-hour clock, so that at 2pm the value of hour is 14.
+  2) Make the program calculate and print the number of seconds since midnight.
+  3) Make the program calculate and print the number of seconds remaining in the day.
+  4) Make the program calculate and print the percentage of the day that has passed.
+  5) Change the values of hour, minute and second to reflect the current time (I assume that some time has elapsed), and check 
+     to make sure that the program works correctly with different values.
+ * 
+ */
+
+using System;
+
+namespace ThinkSharp_Exercise2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int hour = 10; //Step 1 of Exercise 2.3
+            int minute = 36; */
+
+            int hour = 11; //Step 5 of Exercise 2.3
+            int minute = 18;
+
+            int minutesElapsed = (hour * 60) + (minute);
+            Console.WriteLine($"Number of minutes since midnight: {minutesElapsed}"); 
+
+            int secondsElapsed = 60 * ((hour * 60) + (minute));
+            Console.WriteLine($"Number of seconds since midnight: {secondsElapsed}"); //Step 2 of Exercise 2.3
+
+            int secondsRemaining = 86400 - secondsElapsed;
+            Console.WriteLine($"Number of seconds remaining in the day: {secondsRemaining}"); //Step 3 of Exercise 2.3
+
+            int dayPerCent = (secondsElapsed * 100) / 86400;
+            Console.WriteLine($"Percentage of the day that has passed: {dayPerCent}%"); //Step 4 of Exercise 2.3
+        }
+    }
+}
